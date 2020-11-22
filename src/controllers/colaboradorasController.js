a8c77f0d15486e67ad7d1374ee41de836f578b2c
Audry-prog <audryavalos@hotmail.com>
2020-11-22 18:33:24 -0300

    novas correções

--- src/controllers/colaboradorasController.js
+++ src/controllers/colaboradorasController.js
@@ -7,12 +7,10 @@ const create = (req, res) => {
   const senhaComHash = bcrypt.hashSync(req.body.senha, 10);
   req.body.senha = senhaComHash;
   const colaboradora = new colaboradoras(req.body);
-
   colaboradora.save(function(err) {
     if (err) {
       res.status(424).send({ message: err.message })
     }
-
     res.status(201).send(colaboradora.toJSON())
   });
 };
@@ -23,12 +21,10 @@ const getAll = (req, res) => {
     return res.status(401).send('Header não encontrado');
   };
   const token = authHeader.split(' ')[1];
-
   jwt.verify(token, SECRET, err => {
     if (err) {
       return res.status(403).send('Token inválido');
     };
-
     colaboradoras.find((err, colaboradoras) => {
       if(err) {
         return res.status(424).send({ message: err.message });
@@ -43,15 +39,11 @@ const login = (req, res) => {
     if (!colaboradora) {
       return res.status(404).send(`Não existe colaboradora com o email ${req.body.email}`);
     }
-
     const senhaValida = bcrypt.compareSync(req.body.senha, colaboradora.senha);
-
     if (!senhaValida) {
       return res.status(403).send('Que senha é essa doido?');
     }
-
     const token = jwt.sign({ email: req.body.email }, SECRET);
-
     return res.status(200).send(token);
   });
 };
